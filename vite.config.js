e00a3d4071fd28fd6b852b47e77e0ba89bc41dee
Praveen <praveen@getida.com>
2026-04-03 15:56:22 -0400

    frontend deploy and updated vite api

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
-    proxy: {
-      '/api': { target: 'http://backend:4000', changeOrigin: true },
-    },
+    // proxy: {
+    //   '/api': { target: 'http://backend:4000', changeOrigin: true },
+    // },
   },
 });
